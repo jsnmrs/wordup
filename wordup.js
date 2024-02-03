176d449674a4cd7a4594de1823b18112387561fc
Jason Morris <jasonmorris@fastmail.com>
2024-02-03 08:30:37 -0500

    Refactor wordup.js

--- wordup.js
+++ wordup.js
@@ -1,10 +1,87 @@
 /* global CKEDITOR, TurndownService */
-var turndownService = new TurndownService({
+const turndownService = new TurndownService({
   bulletListMarker: "-",
   headingStyle: "atx",
 });
 
-// Configure CKEditor window
+// Module to handle text transformations
+const TextScrubber = (() => {
+  const regexEscape = (string) =>
+    string.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
+
+  const reg = (string) => new RegExp(`https?://${regexEscape(string)}`, "g");
+
+  const scrub = (string) => {
+    return string
+      .replace(
+        /<p><strong>&nbsp;<\/strong><\/p>|<strong>&nbsp;<\/strong>|<p>&nbsp;<\/p>|<h[1-5]>&nbsp;<\/h[1-5]>|<\/strong><strong>|&ndash;ndash;/g,
+        "",
+      )
+      .replace(
+        /<(h[1-5])><strong>(.*?)<\/strong><\/\1>|<(h[1-5])><em>(.*?)<\/em><\/\3>/g,
+        "<$1>$2</$1>",
+      )
+      .replace(/<(table|td|tr|th)\s+width="\d+">/g, "<$1>")
+      .replace(/\n{2,}/g, "\n")
+      .replace(/(&nbsp;){2,}/g, "&nbsp;")
+      .replace(/&nbsp;/g, " ");
+  };
+
+  const addDomainFilter = (string, domain) => string.replace(reg(domain), "");
+
+  const addLinkRel = (string) =>
+    string.replace(
+      /<(a\s+(?:[^>]*?\s+)?href="https?([^"]*)")/g,
+      '<$1 rel="noopener noreferrer"',
+    );
+
+  return {
+    scrub,
+    addDomainFilter,
+    addLinkRel,
+  };
+})();
+
+// Module to manage CKEditor and output operations
+const EditorManager = (() => {
+  const outputTextarea = document.getElementById("output");
+
+  const clearBoth = () => {
+    CKEDITOR.instances.wordup.setData("");
+    outputTextarea.value = "";
+  };
+
+  const wordup = () => {
+    let processedData = TextScrubber.scrub(CKEDITOR.instances.wordup.getData());
+
+    if (
+      document.getElementById("domainfilter").checked &&
+      document.getElementById("domainname").value
+    ) {
+      processedData = TextScrubber.addDomainFilter(
+        processedData,
+        document.getElementById("domainname").value,
+      );
+    }
+
+    if (document.getElementById("linkrel").checked) {
+      processedData = TextScrubber.addLinkRel(processedData);
+    }
+
+    if (document.getElementById("markdown").checked) {
+      processedData = turndownService.turndown(processedData);
+    }
+
+    outputTextarea.value = processedData;
+  };
+
+  return {
+    clearBoth,
+    wordup,
+  };
+})();
+
+// CKEditor configuration
 CKEDITOR.replace("wordup", {
   dataIndentationChars: "  ",
   format_tags: "p;h1;h2;h3;h4;h5",
@@ -12,124 +89,28 @@ CKEDITOR.replace("wordup", {
   removeButtons:
     "Underline,Strike,Subscript,Superscript,Anchor,Styles,Specialchar",
   toolbarGroups: [
-    { groups: ["basicstyles"], name: "basicstyles" },
-    { groups: ["links"], name: "links" },
-    { groups: ["list"], name: "paragraph" },
-    { groups: ["list"], name: "insert" },
-    { groups: ["mode"], name: "document" },
-    { groups: ["styles"], name: "styles" },
+    { name: "basicstyles", groups: ["basicstyles"] },
+    { name: "links", groups: ["links"] },
+    { name: "paragraph", groups: ["list"] },
+    { name: "insert", groups: ["list"] },
+    { name: "document", groups: ["mode"] },
+    { name: "styles", groups: ["styles"] },
   ],
 });
 
-function scrubber(string) {
-  var scrubbed = string;
-
-  scrubbed = scrubbed
-    .replace(/<p><strong>&nbsp;<\/strong><\/p>/g, "")
-    .replace(/<strong>&nbsp;<\/strong>/g, "")
-    .replace(/<p>&nbsp;<\/p>/g, "")
-    .replace(/<h1>&nbsp;<\/h1>/g, "")
-    .replace(/<h2>&nbsp;<\/h2>/g, "")
-    .replace(/<h3>&nbsp;<\/h3>/g, "")
-    .replace(/<h4>&nbsp;<\/h4>/g, "")
-    .replace(/<h5>&nbsp;<\/h5>/g, "")
-    .replace(/<(h[^>])+><strong>(.*)<\/strong><\/h[^>]+>/g, "<$1>$2</$1>")
-    .replace(/<(h[^>])+><em>(.*)<\/em><\/h[^>]+>/g, "<$1>$2</$1>")
-    .replace(/<(table|td|tr|th)\s+width="(\d+?)">/g, "<$1>")
-    .replace(/<\/strong><strong>/g, "")
-    .replace(/&ndash;ndash;/g, "&mdash;")
-    .replace(/\n\n\n/g, "\n\n")
-    .replace(/\n\n/g, "\n")
-    .replace(
-      /&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g,
-      "&nbsp;",
-    )
-    .replace(
-      /&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g,
-      "&nbsp;",
-    )
-    .replace(/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
-    .replace(/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
-    .replace(/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
-    .replace(/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
-    .replace(/&nbsp;&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
-    .replace(/&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
-    .replace(/&nbsp;&nbsp;/g, "&nbsp;")
-    .replace(/&nbsp;/g, " ");
-
-  return scrubbed;
-}
-
-function regexEscape(string) {
-  return string.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
-}
-
-function reg(string) {
-  var escaped = string;
-  var flags = "g";
-
-  escaped = regexEscape(escaped);
-  escaped = "https?://" + escaped;
-
-  return new RegExp(escaped, flags);
-}
-
-function addDomainFilter(string, domain) {
-  var filtered = string;
-  var domainregex = reg(domain);
-
-  filtered = filtered.replace(domainregex, "");
-
-  return filtered;
-}
-
-const wordupEditor = CKEDITOR.instances.wordup;
-const domainFilterCheckbox = document.getElementById("domainfilter");
-const domainNameInput = document.getElementById("domainname");
-const markdownCheckbox = document.getElementById("markdown");
-const outputTextarea = document.getElementById("output");
-const clearButton = document.getElementById("clear");
-const convertButton = document.getElementById("convert");
-const linkRelCheckbox = document.getElementById("linkrel");
-
-function addLinkRel(string) {
-  let plusTarget = string;
-
-  plusTarget = plusTarget.replace(
-    /<(a\s+(?:[^>]*?\s+)?href="https?([^"]*)")/g,
-    '<$1 rel="noopener noreferrer"',
-  );
-
-  return plusTarget;
-}
-
-function clearBoth() {
-  wordupEditor.setData("");
-  outputTextarea.value = "";
-}
-
-function wordup() {
-  const pasteData = wordupEditor.getData();
-
-  let processedData = scrubber(pasteData);
-
-  if (domainFilterCheckbox.checked && domainNameInput.value) {
-    processedData = addDomainFilter(processedData, domainNameInput.value);
-  }
-
-  if (linkRelCheckbox.checked) {
-    processedData = addLinkRel(processedData);
-  }
-
-  if (markdownCheckbox.checked) {
-    processedData = turndownService.turndown(processedData);
-  }
-
-  outputTextarea.value = processedData;
-}
-
-clearButton.addEventListener("click", clearBoth);
-convertButton.addEventListener("click", wordup);
-linkRelCheckbox.addEventListener("click", wordup);
-markdownCheckbox.addEventListener("click", wordup);
-domainFilterCheckbox.addEventListener("click", wordup);
+// Event listeners for UI controls
+document
+  .getElementById("clear")
+  .addEventListener("click", EditorManager.clearBoth);
+document
+  .getElementById("convert")
+  .addEventListener("click", EditorManager.wordup);
+document
+  .getElementById("linkrel")
+  .addEventListener("click", EditorManager.wordup);
+document
+  .getElementById("markdown")
+  .addEventListener("click", EditorManager.wordup);
+document
+  .getElementById("domainfilter")
+  .addEventListener("click", EditorManager.wordup);
